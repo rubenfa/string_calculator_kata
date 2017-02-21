537e7265c9420c09baf988f398f64ec446fb6d44
rubenfa <ruben.fernandez.alfonso@gmail.com>
2017-02-21 14:38:42 +0100

    creating Elixir project

---
+++ lib/string_calculator_kata.ex
@@ -0,0 +1,18 @@
+defmodule StringCalculatorKata do
+  @moduledoc """
+  Documentation for StringCalculatorKata.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> StringCalculatorKata.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/string_calculator_kata_test.exs
@@ -0,0 +1,8 @@
+defmodule StringCalculatorKataTest do
+  use ExUnit.Case
+  doctest StringCalculatorKata
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
